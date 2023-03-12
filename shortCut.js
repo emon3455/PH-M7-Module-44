febe012b0d8fd804c646d241d713b5d5ecb68220
emon3455 <emon.mhk69@gmail.com>
2023-03-13 01:06:36 +0600

    short hand

---
+++ shortCut.js
@@ -0,0 +1,9 @@
+// 1. truthy or falsy:
+
+// 2. ternary operator
+
+// 3. convert string to number or number to string:
+    // we can convert num to string by adding empty string using + operator
+    // we can convert string to number by just suing + before the string variable like this: +number
+
+// 4 sorto sapekkhe function call:
