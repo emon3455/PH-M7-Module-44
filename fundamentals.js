53c770980b30cf6afea53eb3c687b76051b939b9
emon3455 <emon.mhk69@gmail.com>
2023-03-13 00:07:41 +0600

    fundamentals

---
+++ fundamentals.js
@@ -0,0 +1,20 @@
+//1. how to declare variable using let and const
+
+//2=> 6 basics condition( >, <, ===, !==, <=, >=,)
+        // multiple condition && , ||
+
+
+//3. Array:
+
+
+//4. loop:
+
+
+//5. function:
+
+
+//6. object:
+    //3 ways to access object property:
+
+
+
